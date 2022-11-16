87b402c0186a19d062378a57ae2c02f40422b229
milessteamaccount <ronaldlamdev@gmail.com>
2022-11-15 22:01:31 -0600

    created data array

---
+++ src/assets/data/data.ts
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    planName: 'Basic',
+    price: {
+      annual: '$199.99',
+      monthly: '$19.99'
+    },
+    storageSize: '500 GB Storage',
+    usersAllowed: '2 Users Allowed',
+    maxSendSize: 'Send up to 3 GB'
+  },
+  {
+    planName: 'Professional',
+    price: {
+      annual: '$249.99',
+      monthly: '$24.99'
+    },
+    storageSize: '1 TB Storage',
+    usersAllowed: '5 Users Allowed',
+    maxSendSize: 'Send up to 10 GB'
+  },
+  {
+    planName: 'Master',
+    price: {
+      annual: '$399.99',
+      monthly: '$39.99'
+    },
+    storageSize: '2 TB Storage',
+    usersAllowed: '10 Users Allowed',
+    maxSendSize: 'Send up to 20 GB'
+  }
+]
