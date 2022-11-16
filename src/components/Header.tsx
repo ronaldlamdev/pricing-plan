83f23131e12881f8340bf049680578b039bfcc1a
milessteamaccount <ronaldlamdev@gmail.com>
2022-11-15 19:44:41 -0600

    customized css code of toggle button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,8 @@ const Header = () => {
       <h1>Our Pricing</h1>
       <div>
         <span>Annually</span>
-        <label className="switch">
-          <input type="checkbox" />
+        <label className="relative inline-block w-[3.75rem] h-[2.125rem]">
+          <input type="checkbox" className="opacity-0" />
           <span className="slider round"></span>
         </label>
         <span>Monthly</span>
