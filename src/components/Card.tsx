6a381ee12c7c6ab436725c099781a1fc7730d6df
milessteamaccount <ronaldlamdev@gmail.com>
2022-11-16 21:27:57 -0600

    setup html structure of card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,32 @@
 import { data } from "../assets/data/data"
 
 const Card = () => {
+
+  const basic = data[0]
+  const professional = data[1]
+  const master = data[2]
+
   return (
     <div>
-      
+
+      <div>
+        <h1>{basic.planName}</h1>
+        <h1>{basic.price.annual}</h1>
+      </div>
+
+      <div>
+        <div>
+          <h2>{basic.storageSize}</h2>
+        </div>
+        <div>
+          <h2>{basic.usersAllowed}</h2>
+        </div>
+        <div>
+          <h2>{basic.maxSendSize}</h2>
+        </div>
+      </div>
+
+      <button>Learn More</button>
     </div>
   )
 }
