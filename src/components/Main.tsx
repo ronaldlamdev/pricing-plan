fb291b4dc75c95eeebf9acccf728d6d2dac2fba3
milessteamaccount <ronaldlamdev@gmail.com>
2022-11-18 16:07:53 -0600

    mapped out data.ts to card component and props

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,17 @@
 import Card from "./Card"
+import { data } from '../assets/data/data';
 
 const Main = () => {
   return (
     <main className="px-4 py-8 flex flex-col items-center">
-      <Card />
+      {data.map((plan) => (
+        <Card name={plan.planName}
+              price={plan.price.annual}
+              size={plan.storageSize}
+              users={plan.usersAllowed}
+              maxSize={plan.maxSendSize}
+        />
+      ))}
     </main>
   )
 }
