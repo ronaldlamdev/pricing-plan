87f1e7ee53583a89a3c4dd151078314ef7d36a00
milessteamaccount <ronaldlamdev@gmail.com>
2022-11-18 15:51:29 -0600

    defined interface for props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,30 @@
-import { data } from "../assets/data/data"
+interface dataProp {
+  name: string,
+  price: string,
+  size: string,
+  users: string,
+  maxSize: string
+}
 
-const Card = () => {
-
-  const basic = data[0]
+const Card = ({name, price, size, users, maxSize}: dataProp) => {
 
   return (
     <div className="light-card">
 
       <div className="pt-5 pb-6">
-        <h1 className="text-[#6d708d] text-lg mb-5">{basic.planName}</h1>
-        <h1 className="text-6xl text-[#494c5f] flex items-center"><span className="text-4xl">$</span>{basic.price.annual}</h1>
+        <h1 className="text-[#6d708d] text-lg mb-5">{name}</h1>
+        <h1 className="text-6xl text-[#494c5f] flex items-center"><span className="text-4xl">$</span>{price}</h1>
       </div>
 
       <div className="text-[#6d708d] text-lg py-4 w-full px-12">
         <div className="py-4 border-y flex flex-col justify-center">
-          <h2>{basic.storageSize}</h2>
+          <h2>{size}</h2>
         </div>
         <div className="py-4 border-y flex flex-col justify-center">
-          <h2>{basic.usersAllowed}</h2>
+          <h2>{users}</h2>
         </div>
         <div className="py-4 border-y flex flex-col justify-center">
-          <h2>{basic.maxSendSize}</h2>
+          <h2>{maxSize}</h2>
         </div>
       </div>
 
