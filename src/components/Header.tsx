da093f02dd6d76c19892fe9469b9c98624db567b
milessteamaccount <ronaldlamdev@gmail.com>
2022-11-19 14:44:43 -0600

    finished styling on larger screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="px-4 py-12 w-full flex flex-col justify-center items-center">
+    <header className="px-4 py-12 w-full flex flex-col justify-center items-center lg:mt-8">
       <h1 className="text-[#6d708d] text-3xl">Our Pricing</h1>
       <div className="w-full mt-12 max-w-[300px] flex justify-between items-center px-4">
         <span className="text-[#b3b5c6]">Annually</span>
